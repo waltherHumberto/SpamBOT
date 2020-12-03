e6bf8679ac7205f9c2e1916ac05e9145955bc8c6
walther humbero <waltherhumberto@gmail.com>
2020-12-03 19:52:21 -0200

    Programa funcional V1.0

--- spambot.py
+++ spambot.py
@@ -2,6 +2,7 @@
 from PyQt5 import QtWidgets, uic
 import time
 import pyautogui
+import keyboard
 import sys
 
 class Ui(QtWidgets.QMainWindow):
@@ -9,20 +10,30 @@ class Ui(QtWidgets.QMainWindow):
         super(Ui, self).__init__()
         uic.loadUi('spaminterface.ui', self)
         self.show()
-        self.pushButton.clicked.connect(lambda : self.rodabot()) # Inicia a conexão do botão inicia com o evento 
+        self.teladousuario.clear()
+        self.teladousuario.append("Clique no botão iniciar para começar a spamar") 
+        self.teladousuario.append("Você tem 4 segundos para clicar no local desejado") 
+        self.pushButton.clicked.connect(lambda: self.rodabot()) # Inicia a conexão do botão inicia com o evento 
 
     def rodabot(self):
-        # while(PyQt5.QKeyEvent() != ESC):
-            time.sleep(3) # tempo de espera para o usuario clicar na caixa de mensagem
-            f = self.lineEdit.text() 
-            print(f)
-            # while(True)
-            for word in f :
-                print("To mandando essa merda")
-                pyautogui.typewrite(word)
-                pyautogui.press('enter')
-
-
+        print("entrou na funçao")
+        
+    
+        time.sleep(4) # tempo de espera para o usuario clicar na caixa de mensagem
+        while(not keyboard.is_pressed('ESC')):
+            with  open("texto.txt","r") as f:
+                
+                for word in f :
+                    self.teladousuario.append("To mandando essa merda")
+                    print(word)
+                    pyautogui.typewrite(word)
+                    pyautogui.press('enter')
+        self.teladousuario.append("parei de mandar ")
+        return 
+        
+    def sair():
+        quit()
+    
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
 app.exec_()
